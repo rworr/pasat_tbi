cf54ea98271b0115e78a88bf7263579b5106ce2d
Ryan Orr <rworr@uwaterloo.ca>
2017-04-16 03:28:08 -0400

    Basic sequence information

--- pasat.py
+++ pasat.py
@@ -23,6 +23,9 @@ for i in range(0, 9):
         added_keys.append(ni+nj)
         summed_keys.append(number_keys[i+j+1])
 
+number_vocab.parse('POS1')
+pos_next = number_vocab.add('POSN', number_vocab.create_pointer(unitary=True))
+
 current_input = '0'
 def number_input(t):
     global current_input
@@ -30,14 +33,29 @@ def number_input(t):
     isi_t = (t % isi)
     if isi_t < 0.002:
         current_input = number_keys[np.random.randint(0, 9)]
-    elif (t % isi) <= 0.2:
+    elif isi_t <= 0.2:
         return current_input
     return '0'
+    
+def control(t):
+    if t <= 0.2:
+        return 'INIT'
+    elif (t % isi) <= 0.2:
+        return 'INPUT'
+    else:
+        return 'NONE'
+
+def position(t):
+    result = 'POS1'
+    for i in range(0, int(t // isi)):
+        result += ' * POSN'
+    return result
 
 with spa.SPA('AdditionMemory', seed=1) as model:
     # input
-    model.number_in = spa.Buffer(dimensions=dim)
-    model.inp = spa.Input(number_in=number_input)
+    model.number_in = spa.State(dimensions=dim)
+    model.position = spa.State(dimensions=dim)
+    model.inp = spa.Input(number_in=number_input, position=position)
     
     # addition associative memory
     model.assoc_mem = spa.AssociativeMemory(input_vocab=number_vocab,
@@ -49,8 +67,9 @@ with spa.SPA('AdditionMemory', seed=1) as model:
                                             threshold=0.3)
     
     # working memory
-    model.mem = spa.Memory(dimensions=dim, subdimensions=16, 
-                           synapse=0.1, neurons_per_dimension=50)
+    model.seq_mem = spa.State(dimensions=dim, 
+                              subdimensions=16, neurons_per_dimension=50,
+                              feedback=1.0, feedback_synapse=0.1)
 
     # WTA memories to pull out 2 most recent items
     model.number_one = spa.AssociativeMemory(input_vocab=number_vocab,
@@ -59,7 +78,7 @@ with spa.SPA('AdditionMemory', seed=1) as model:
                                              output_keys=number_keys,
                                              wta_output=True,
                                              wta_synapse=0.005,
-                                             threshold=0.7)
+                                             threshold=0.3)
                                              
     model.number_two = spa.AssociativeMemory(input_vocab=number_vocab,
                                              output_vocab=number_vocab,
@@ -69,12 +88,21 @@ with spa.SPA('AdditionMemory', seed=1) as model:
                                              wta_synapse=0.005,
                                              threshold=0.3)
 
+    model.number_buf = spa.State(dimensions=dim)
+    model.memory_buf = spa.State(dimensions=dim)
+    model.prev_position = spa.State(dimensions=dim)
+    
     cortical_actions = spa.Actions(
-        'mem = number_in',
-        'number_one = mem',
-        'number_two = mem - number_one',
+        'seq_mem = number_in * position',
+        'number_one = seq_mem * ~position',
+        'number_buf = number_one * position',
+        'memory_buf = seq_mem - number_buf',
+        'prev_position = position * ~POSN',
+        'number_two = memory_buf * ~prev_position',
         'assoc_mem = number_one * number_two',
     )
     
     model.cortical = spa.Cortical(cortical_actions)
-
+    
+    #model.bg = spa.BasalGanglia(actions)
+    #model.thal = spa.Thalamus(model.bg)
