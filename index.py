3190e4932e801eeb738daea3596e61c049c40440
Ryan Orr <rworr@uwaterloo.ca>
2017-05-03 17:04:42 -0400

    Using InputGatedMemory

--- index.py
+++ index.py
@@ -2,13 +2,38 @@ import nengo
 import numpy as np
 
 from nengo import spa
+from nengo.networks import InputGatedMemory
 
 dim = 32
+isi = 0.6
 
 vocab = spa.Vocabulary(dim)
-vocab.parse('POSITION')
+vocab.parse('POS1')
 vocab.add('NEXT', vocab.create_pointer(unitary=True))
+vocab.add('POS2', vocab.parse('POS1 * NEXT'))
+
+def clock(t):
+    if (t % isi) < 0.3:
+        return 0
+    return 1
+
+def pos(t):
+    if t < 0.6:
+        return 'POS1'
+    if t < 1.2:
+        return 'POS2'
+    return '0'
 
 with spa.SPA('Indexing') as model:
-    osc = nengo.Ensemble(100, 2)
-    nengo.Connection(osc, osc, transform=[[1, -0.1], [0.1, 1]])
+    model.clock = nengo.Node(clock)
+    model.position = spa.State(dim)
+    model.output = spa.State(dim)
+    model.inp = spa.Input(position=pos)
+    
+    
+    model.current = InputGatedMemory(200, dim)
+    
+    nengo.Connection(model.clock, model.current.gate)
+    nengo.Connection(model.position.output, model.current.input)
+    nengo.Connection(model.current.output, model.output.input)
+    
